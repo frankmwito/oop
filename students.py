9341ebd75e242f24a8b428bf205d95b7e1f41242
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-13 12:58:43 +0300

    class student with methods to add grades and calculate the average grade

--- students.py
+++ students.py
@@ -1,1 +1,25 @@
 # class student with methods to add grades and calculate the average grade
+
+class student:
+    def __init__(self, name):
+        self.name = name
+        self.grades = []
+        
+    def average_grade(self):
+        subjects = int(input("Enter the size of grades list: "))
+        
+        for i in range(subjects):
+            self.grades.append(float(input(f"Enter the grades at {i+1}: ")))
+            
+        sum = 0
+        for grade in self.grades:
+            sum += grade
+            
+        average = sum/len(self.grades)
+        
+        print(f"{self.name} your average grade is: {average}")
+        
+        
+student1 = student(input("Enter your name: "))
+
+student1.average_grade()
