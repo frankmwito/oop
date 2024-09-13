a9b3b7fc3402ab4b09edf7a659274d093cc2607f
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-13 22:42:17 +0300

    Library system

---
+++ Library system.py
@@ -0,0 +1,3 @@
+# Define a class library that tracks the books available in a library
+# create methods add_biik() to add books to the library, borrow_book() to borrow books(if available), and return_book() to return a borrowed book.
+# use a list to store the books available and a dictionary to track borrowed books.
