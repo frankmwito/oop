db56fd39e89c910bbf20fbf8f86d4fe7d7edf2fb
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-13 12:43:17 +0300

    greeting with class person with attributes name and age.

--- greet.py
+++ greet.py
@@ -1,2 +1,15 @@
 # class person with attributes name and age.
 # add method greet() that prints a greeting message using the person's name
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def greet(self):
+        print(f"Hello {self.name} your are {self.age} years old")
+        
+        
+person1 =  Person(input("Enter your name: "), int(input("Enter your age: ")))
+
+person1.greet()
